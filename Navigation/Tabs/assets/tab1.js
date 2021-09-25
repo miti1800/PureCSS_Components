8de16b2e37077b1e077a4a441dcdf9c5cd6832c8
miti1800 <mitiupadhyay1800@gmail.com>
2021-09-25 17:45:07 +0530

    Updated tab1

--- Navigation/Tabs/assets/tab1.js
+++ Navigation/Tabs/assets/tab1.js
@@ -1,15 +1,14 @@
-function openPage(pageName,elmnt) {
+function openPage(pageName,id) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
+  tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablink");
+  
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
   document.getElementById(pageName).style.display = "block";
-  document.getElementById(pageName).style.display = "block";
+  tablinks[0].classList.remove("default");
 }
-
-document.getElementById("defaultOpen").click();
