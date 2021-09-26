83c69d37ffc3c93b7e3d4c83c6cc9b7aa3dc32c0
miti1800 <mitiupadhyay1800@gmail.com>
2021-09-26 22:50:19 +0530

    updated tab.js

--- Navigation/Tabs/assets/tab.js
+++ Navigation/Tabs/assets/tab.js
@@ -5,11 +5,6 @@ function openPage(pageName,id) {
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
   document.getElementById(pageName).style.display = "block";
   tablinks[0].classList.remove("default");
-  id.style.backgroundColor = "#6CB4EE";
 }
