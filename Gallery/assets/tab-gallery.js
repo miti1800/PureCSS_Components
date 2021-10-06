2db9fc7a73db2b08fba8e4cdfb32e03d4f173b32
miti1800 <mitiupadhyay1800@gmail.com>
2021-10-06 21:32:52 +0530

    updated tab gallery

--- Gallery/assets/tab-gallery.js
+++ Gallery/assets/tab-gallery.js
@@ -1,5 +1,9 @@
+oldimgs = document.getElementById("first-image");
 function expand(imgs) {
+  oldimgs.parentElement.classList.remove("active");
+  imgs.parentElement.classList.add("active");
   var openImg = document.getElementById("expandedImg");
   openImg.src = imgs.src;
   openImg.parentElement.style.display = "block";
+  oldimgs = imgs
 }
