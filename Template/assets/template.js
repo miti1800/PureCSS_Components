6324faca7b32cb6444acdbdc768c093bdeb00bcf
miti1800 <mitiupadhyay1800@gmail.com>
2021-12-01 16:02:08 +0530

    Final Template

--- Template/assets/template.js
+++ Template/assets/template.js
@@ -1,3 +1,46 @@
+const sc = document.querySelector(".stepper_content").children;
+const scount = document.querySelector(".stepper_count").children;
+const accordian = document.getElementsByClassName('content');
+for(let k=0;k<accordian.length;k++){
+    accordian[k].addEventListener('click',function(){
+        this.classList.toggle('active')
+    })
+}
+
+function openPage(pageName,id) {
+    var tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    tablinks = document.getElementsByClassName("tablink");
+    for (let j = 0; j < tabcontent.length; j++) {
+      tabcontent[j].style.display = "none";
+    }
+    document.getElementById(pageName).style.display = "flex";
+    tablinks[0].classList.remove("default");
+}
+
+var index = 0;
+function nextBtn() {
+    sc[index].classList.remove("show");
+    scount[index].classList.add("complete");
+    scount[index].classList.remove("active");
+    index++;
+    sc[index].classList.add("show");
+    scount[index].classList.add("active");
+}
+function prevBtn() {
+    sc[index].classList.remove("show");
+    index--;
+    sc[index].classList.add("show");
+    scount[index].classList.add("active");
+}
+function submitBtn() {
+    sc[index].classList.remove("show");
+    scount[index].classList.remove("active");
+    scount[index].classList.add("complete");
+    index++;
+    sc[index].classList.add("show");
+}
+
 var i = 0;
 function openMenu() {
     document.querySelector(".navbar .right-part ul").classList.add("open");
@@ -9,51 +52,4 @@ function closeMenu() {
     document.querySelector(".navbar .right-part ul").classList.add("close");
     document.querySelector(".navbar .right-part ul").classList.remove("open");
     i = 1;
-}
-
-function openPage(pageName,id) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    document.getElementById(pageName).style.display = "flex";
-    tablinks[0].classList.remove("default");
-}
-
-const accordian = document.getElementsByClassName('content');
-for(i=0;i<accordian.length;i++){
-    accordian[i].addEventListener('click',function(){
-        this.classList.toggle('active')
-    })
-}
-
-const sc = document.querySelector(".stepper_content").children;
-const scount = document.querySelector(".stepper_count").children;
-var i = 0;
-
-function nextBtn() {
-    console.log(i);
-    sc[i].classList.remove("show");
-    scount[i].classList.add("complete");
-    scount[i].classList.remove("active");
-    i++;
-    sc[i].classList.add("show");
-    scount[i].classList.add("active");
-}
-
-function prevBtn() {
-    sc[i].classList.remove("show");
-    i--;
-    sc[i].classList.add("show");
-    scount[i].classList.add("active");
-}
-
-function submitBtn() {
-    sc[i].classList.remove("show");
-    scount[i].classList.remove("active");
-    scount[i].classList.add("complete");
-    i++;
-    sc[i].classList.add("show");
 }
